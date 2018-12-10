84462bfed2bcb54452601687f3a2fc87aef5f0ae
Balavishn <37904008+Balavishn@users.noreply.github.com>
2018-12-10 15:27:50 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class check{
+
+public static void main(String[] args){
+
+Scanner s = new Scanner(System.in);
+System.out.println("Enter Number:");
+int n = s.nextInt();
+if(n<0){
+System.out.println("Positive");
+}
+elseif(n<0){
+System.out.println("Negative");
+}
+else{
+System.out.println("Zero");
+}
+}
+}
